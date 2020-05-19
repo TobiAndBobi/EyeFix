d5a28d1dda7a0dc13fd0272c8a5a27310a38501b
bharat <bharat.goel@cheenilabs.com>
2020-05-19 11:03:27 -0400

    changes

--- server/server.py
+++ server/server.py
@@ -42,8 +42,8 @@ def submitUserPreferences():
         fix_2 = ga.fixation_plot(dict_2,"algorithm2")
 
     output["gazeAndDensity"]=dict_1+dict_2
-    # fix_ = ga.normalize("userImage/" + uploadEyeGazeImage.filename,fix_2+fix_1)
-    fix_ = fix_1+fix_2
+    fix_ = ga.normalize("userImage/" + uploadEyeGazeImage.filename,fix_2+fix_1)
+    # fix_ = fix_1+fix_2
     output["fixationPlot"] = fix_
     # print (output['gazeAndDensity'])
     response = jsonify(output)
